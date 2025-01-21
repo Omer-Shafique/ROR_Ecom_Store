d1f3c5d98d7ed380febb0aefe52cd12f8f0e5017
Omer Shafique <omer.shafiqu3@gmail.com>
2025-01-21 20:52:48 +0500

    cleaned the controller/concern code

--- app/controllers/concerns/user_authorizable.rb
+++ app/controllers/concerns/user_authorizable.rb
@@ -9,8 +9,6 @@ module UserAuthorizable
 
   def correct_user
     @product = current_user.products.find_by(id: params[:id])
-    
-    # Check if the current user is an admin or the owner of the product
     if @product.nil? || (!current_user.admin? && @product.user != current_user)
       redirect_to products_path, notice: "Not authorized to edit this product"
     end
